9c576830ea0f6e8dd5c1935c471cfaaaeeb8e278
Murad Ahmed <murad15-9149@diu.edu.bd>
2021-02-06 21:05:03 +0600

    true false value

---
+++ true-false.js
@@ -0,0 +1,26 @@
+//value that are true
+let userName = true;
+    userName = " ";
+    userName = 122;
+//value that are false
+let UserAge;
+    UserAge = false;
+    UserAge = null;
+    UserAge = undefined;
+    UserAge = NaN;
+    UserAge = 0;
+    if(UserAge){
+        console.log("This is true value");
+    }
+    else{
+        console.log("This is false value");
+    }
+
+    if(userName){
+        console.log("This is true value");
+    }
+
+    else{
+        console.log("This is false value");
+    }
+
